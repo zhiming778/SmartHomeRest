f5e58e55aa7e257c9f19f5957bc0aa5c959a9ad1
Jimmy Ong <zhimingweng778@gmail.com>
2021-11-04 19:30:01 -0400

    automatically load data to H2 database

--- src/main/java/com/jimmy/util/Messages.java
+++ src/main/java/com/jimmy/util/Messages.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.jimmy.dao.MessageEntityRepository;
@@ -17,11 +14,10 @@ public class Messages {
 
     private Map<String, String> map;
 
-    @Autowired
     private MessageEntityRepository repository;
 
-    @PostConstruct
-    public void init() {
+    public Messages(MessageEntityRepository repository) {
+        this.repository = repository;
         map = new HashMap<>();
         List<MessageEntity> list = repository.findAll();
         System.out.println(list.size());
